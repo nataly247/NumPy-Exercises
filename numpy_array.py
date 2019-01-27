09e1a48127ebd5f0d0b75fa437b6ffbd19a39869
nataly247 <nataly247@gmail.com>
2019-01-27 22:42:54 +0200

    numpy arrats

---
+++ numpy_array.py
@@ -0,0 +1,22 @@
+## TASK ##
+## A list baseball has already been defined in the Python script, 
+## representing the height of some baseball players in centimeters.
+##
+## Import the numpy package as np, so that you can refer to numpy with np.
+##
+## Use np.array() to create a numpy array from baseball. Name this array np_baseball.
+##
+## Print out the type of np_baseball to check that you got it right.
+######
+
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
